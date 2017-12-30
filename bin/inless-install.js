5a41608c41f3be5495919113a54aa0edcb6b9386
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2017-12-30 07:10:03 +0200

    Don't run installation if it's not in a node_modules

--- bin/inless-install.js
+++ bin/inless-install.js
@@ -5,17 +5,24 @@ const fse = require('fs-extra');
 const path = require('path');
 
 const DEFAULT_DIR = 'dist';
-
 const workDir = process.cwd();
-const pkgFilename = 'package.json';
-const pkg = require(path.join(workDir, pkgFilename));
 
-const distDirname = (pkg.inless && pkg.inless.dist) || DEFAULT_DIR;
-const files = fs.readdirSync(workDir);
+const cleanDir = (dir, exclude) => {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        if (exclude.indexOf(file) === -1) fse.removeSync(path.join(workDir, file));
+    });
+};
 
-const exclude = [pkgFilename, distDirname, '.git', '.gitignore', 'node_modules', 'yarn.lock', 'package.lock'];
+const install = () => {
+    const pkgFilename = 'package.json';
+    const pkg = require(path.join(workDir, pkgFilename));
 
-files.forEach(file => {
-    if (exclude.indexOf(file) === -1) fse.removeSync(path.join(workDir, file));
-});
-fse.moveSync(path.join(workDir, distDirname), workDir);
+    const distDirname = (pkg.inless && pkg.inless.dist) || DEFAULT_DIR;
+    const exclude = [pkgFilename, distDirname, '.git', '.gitignore', 'node_modules', 'yarn.lock', 'package.lock'];
+
+    cleanDir(workDir, exclude);
+    fse.moveSync(path.join(workDir, distDirname), workDir);
+};
+
+if (path.basename(path.dirname(workDir)) === 'node_modules') install();
